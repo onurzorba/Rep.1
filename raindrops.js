c9d339a20ee0ea16ea34466f36d3b09b69aa4c5b
onurzorba <128971767+onurzorba@users.noreply.github.com>
2023-04-10 00:12:12 +0200

    Update raindrops.js

--- raindrops.js
+++ raindrops.js
@@ -1,66 +1,67 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-let drops = [];
+let canvas, ctx;
+let raindrops = [];
+let lightningTimer = 0;
+let lightningActive = false;
+let lightningOpacity = 0;
 
-class Drop {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.len = Math.random() * 80 + 80;
-    this.speed = Math.random() * 2 + 3;
-    this.size = Math.random() * 3 + 1;
-  }
-
-  update() {
-    this.y += this.speed;
-
-    if (this.y > canvas.height) {
-      this.y = Math.random() * -canvas.height;
-      this.x = Math.random() * canvas.width;
-    }
-  }
-
-  draw() {
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x, this.y + this.len);
-    ctx.strokeStyle = "#03a9f4";
-    ctx.lineWidth = this.size;
-    ctx.stroke();
+function createRaindrops() {
+  for (let i = 0; i < 100; i++) {
+    let raindrop = {
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      length: Math.random() * 50 + 10,
+      speed: Math.random() * 10 + 10
+    };
+    raindrops.push(raindrop);
   }
 }
 
-function createDrops(num) {
-  for (let i = 0; i < num; i++) {
-    drops.push(new Drop());
+function animateRaindrops() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < raindrops.length; i++) {
+    let raindrop = raindrops[i];
+    ctx.beginPath();
+    ctx.moveTo(raindrop.x, raindrop.y);
+    ctx.lineTo(raindrop.x + raindrop.length * Math.sin(Math.PI/3), raindrop.y + raindrop.length * Math.cos(Math.PI/3));
+    ctx.strokeStyle = 'rgba(174, 194, 224, 0.5)';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    raindrop.y += raindrop.speed;
+    if (raindrop.y > canvas.height) {
+      raindrop.y = -50;
+    }
+  }
+}
+
+function animateLightning() {
+  ctx.fillStyle = `rgba(128, 0, 128, ${lightningOpacity})`;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  lightningTimer--;
+  if (lightningTimer <= 0 && !lightningActive) {
+    lightningActive = true;
+    lightningOpacity = 1;
+    lightningTimer = Math.floor(Math.random() * 200) + 100;
+  }
+  else if (lightningTimer <= 0 && lightningActive) {
+    lightningActive = false;
+    lightningOpacity = 0;
+    lightningTimer = Math.floor(Math.random() * 2000) + 1000;
   }
 }
 
 function animate() {
+  animateRaindrops();
+  animateLightning();
   requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  for (let i = 0; i < drops.length; i++) {
-    drops[i].update();
-    drops[i].draw();
-  }
 }
 
-createDrops(50);
-animate();
-
-function createLightning() {
-  const flash = document.createElement("div");
-  flash.classList.add("flash");
-  document.body.appendChild(flash);
-  setTimeout(() => {
-    flash.remove();
-  }, 500);
+function init() {
+  canvas = document.getElementById('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx = canvas.getContext('2d');
+  createRaindrops();
+  animate();
 }
 
-setInterval(() => {
-  const chance = Math.random();
-  if (chance < 0.05) {
-    createLightning();
-  }
-}, 5000);
+window.onload = init;
